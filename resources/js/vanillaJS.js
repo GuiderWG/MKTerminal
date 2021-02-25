f240109affb2acd49a8b8f5d6a43b49069ff8c3b
Vladimir <guiderwg@gmail.com>
2021-02-25 08:31:53 +0400

    Preloader mobile

--- resources/js/vanillaJS.js
+++ resources/js/vanillaJS.js
@@ -1,3 +1,130 @@
+export function preloaderAnimation() {
+  let preloaderStart = document.getElementById('preloader-container');
+
+  const preloader = document.querySelectorAll('.preloader__container');
+  const preloaderBar = document.querySelectorAll('.preloader-load .preloader-load__bar');
+  const preloaderBarEmpty = document.querySelectorAll('.preloader-load .preloader-load__bar_empty');
+  const preloaderIcon = document.querySelectorAll('.preloader-load .preloader-load__icon');
+  const preloaderLetter = document.querySelectorAll('.preloader__title .letter');
+  const preloaderCluster = document.querySelectorAll('.preloader-bg .preloader-bg__cluster');
+
+  if (preloaderStart) {
+    document.body.classList.add('preloader');
+
+    function startPreloader() {
+      preloaderStart.classList.add('preloader__container_show');
+      anime
+        .timeline({
+          easing: 'spring(1, 40, 10, 0)',
+          duration: 1600,
+        })
+        .add(
+          {
+            targets: preloaderBarEmpty,
+            opacity: [0, 1],
+          },
+          500
+        )
+        .add(
+          {
+            targets: preloaderLetter,
+            opacity: 1,
+            translateY: -50,
+            delay: anime.stagger(100, { start: 1000 }),
+            translateX: [-10, 0],
+          },
+          500
+        )
+        .add(
+          {
+            targets: preloaderCluster,
+            opacity: 1,
+            easing: 'spring(1, 30, 10, 0)',
+            delay: anime.stagger(100, { start: 1000 }),
+            translateY: ['100%', 0],
+          },
+          500
+        )
+        .add(
+          {
+            targets: preloaderBar,
+            width: [
+              { value: '10%', duration: 400 },
+              { value: '30%', duration: 400, delay: 500 },
+              { value: '60%', duration: 400, delay: 500 },
+              { value: '100%', duration: 400, delay: 500 },
+            ],
+            direction: 'alternate',
+            easing: 'spring(1, 40, 10, 0)',
+            delay: function (el, i) {
+              return i * 300;
+            },
+            endDelay: function (el, i, l) {
+              return (l - i) * 300;
+            },
+          },
+          500
+        );
+
+      anime({
+        targets: preloaderIcon,
+        opacity: [0, 1],
+        translateX: [
+          { value: '-30%', duration: 1500 },
+          { value: '30%', duration: 1500, delay: 500 },
+          { value: '80%', duration: 1500, delay: 500 },
+          { value: '100%', duration: 1500, delay: 500 },
+        ],
+        direction: 'alternate',
+        loop: true,
+        easing: 'spring(1, 40, 10, 0)',
+        delay: function (el, i, l) {
+          return i * 200;
+        },
+        endDelay: function (el, i, l) {
+          return (l - i) * 200;
+        },
+      });
+    }
+
+    startPreloader();
+
+    function fadeOutPreloader(el) {
+      document.body.classList.remove('preloader');
+      setTimeout(() => {
+        anime
+          .timeline({
+            easing: 'easeOutSine',
+            duration: 600,
+          })
+          .add(
+            {
+              targets: preloader,
+              height: ['100%', '0%'],
+              top: ['50%'],
+            },
+            200
+          )
+          .add(
+            {
+              complete: function (anim) {
+                preloaderStart.classList.remove('preloader__container_show');
+              },
+            },
+            300
+          );
+      }, 1000);
+    }
+
+    window.onload = function () {
+      setTimeout(function () {
+        fadeOutPreloader(preloaderStart);
+        logoPattern();
+      }, 2000);
+    };
+  }
+}
+
 export default document.addEventListener('DOMContentLoaded', () => {
   /*Поиск в шапке*/
   (() => {
@@ -324,97 +451,6 @@ export default document.addEventListener('DOMContentLoaded', () => {
           begin: randomValuesOpTran,
         });
     },
-
-    /*preloaderAnimation: () => {
-    const preloader = document.querySelectorAll('.preloader__container');
-    const preloaderBar = document.querySelectorAll('.preloader-load .preloader-load__bar');
-    const preloaderBarEmpty = document.querySelectorAll('.preloader-load .preloader-load__bar_empty');
-    const preloaderIcon = document.querySelectorAll('.preloader-load .preloader-load__icon');
-    const preloaderLetter = document.querySelectorAll('.preloader__title .letter');
-    const preloaderCluster = document.querySelectorAll('.preloader-bg .preloader-bg__cluster');
-
-    anime
-      .timeline({
-        easing: 'spring(1, 40, 10, 0)',
-        duration: 1600,
-      })
-      .add(
-        {
-          targets: preloader,
-          opacity: [0, 1],
-          borderRadius: ['50%', '0%'],
-        },
-        1200
-      )
-      .add(
-        {
-          targets: preloaderBarEmpty,
-          opacity: [0, 1],
-        },
-        1800
-      )
-      .add(
-        {
-          targets: preloaderLetter,
-          opacity: 1,
-          translateY: -50,
-          delay: anime.stagger(100, { start: 1000 }),
-          translateX: [-10, 0],
-        },
-        1800
-      )
-      .add(
-        {
-          targets: preloaderCluster,
-          opacity: 1,
-          easing: 'spring(1, 30, 10, 0)',
-          delay: anime.stagger(100, { start: 1000 }),
-          translateY: ['100%', 0],
-        },
-        1800
-      )
-      .add(
-        {
-          targets: preloaderBar,
-          // width: [0, 1980],
-          width: [
-            { value: '10%', duration: 400 },
-            { value: '30%', duration: 400, delay: 500 },
-            { value: '60%', duration: 400, delay: 500 },
-            { value: '100%', duration: 400, delay: 500 },
-          ],
-          direction: 'alternate',
-          easing: 'spring(1, 40, 10, 0)',
-          delay: function (el, i) {
-            return i * 300;
-          },
-          endDelay: function (el, i, l) {
-            return (l - i) * 300;
-          },
-        },
-        1900
-      );
-
-    anime({
-      targets: preloaderIcon,
-      opacity: [0, 1],
-      translateX: [
-        { value: '-30%', duration: 1500 },
-        { value: '30%', duration: 1500, delay: 500 },
-        { value: '80%', duration: 1500, delay: 500 },
-        { value: '100%', duration: 1500, delay: 500 },
-      ],
-      direction: 'alternate',
-      loop: true,
-      easing: 'spring(1, 40, 10, 0)',
-      delay: function (el, i, l) {
-        return i * 200;
-      },
-      endDelay: function (el, i, l) {
-        return (l - i) * 200;
-      },
-    });
-  }*/
   };
 
   window.logoPattern = commonAnimation.logoPattern;
@@ -423,11 +459,9 @@ export default document.addEventListener('DOMContentLoaded', () => {
   window.rightSide = commonAnimation.rightSide;
   window.downSide = commonAnimation.downSide;
   window.mapDots = commonAnimation.mapDots;
-  //window.preloaderAnimation = commonAnimation.preloaderAnimation;
 
-  logoPattern();
+  //logoPattern();
   downSide();
-  //preloaderAnimation();
 
   (() => {
     function Particles() {
